5156df2883d78c2f181483a0eb52aa097553152d
Kevin Murray <kdmfoss@gmail.com>
2017-06-20 12:10:20 +1000

    Join data with caroline's metadata

--- metadata/reformat-metadata.R
+++ metadata/reformat-metadata.R
@@ -41,3 +41,16 @@ t = pellie %>%
     do(writeaxe(.))
 
 write.csv(pellie, "pellie-metadata.csv", row.names=F)
+
+
+### new data from caroline
+cc = read.csv("original/scabrum.GPS.csv", skip=2, stringsAsFactors=F)
+samp = read.csv("pellie-metadata.csv", stringsAsFactors=F)
+# Not these, it's invalid for the GH plates
+# samp = read.csv("pellie-metadata.csv", stringsAsFactors=F) %>%
+#               extract(plate.code, into=c("plate.num"), regex="(\\d+)", remove=F) %>%
+#               mutate(plate.num = as.numeric(plate.num))
+# samp2 = left_join(samp, cc, by=c("plate.num"="Plate", "well"="Well"))
+samp2 = left_join(samp, cc, by=c("accession"="DNA_CODE"))
+
+write.csv(samp2, "pellie-metadata-with-cc.csv", row.names=F)
